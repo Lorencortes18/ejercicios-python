5fc6d8865ed1ddf6a9fc19ed2d0e2e62ace5a87c
Lorencortes18 <lorenacortesacevedo@gmail.com>
2024-07-29 17:15:27 -0500

    subiendo archivos al repositorio

---
+++ ejercicio.py
@@ -0,0 +1,24 @@
+mensaje="hola,estoy aprendiendo python"
+nombre="lorena"
+formacion="ADSO"
+Edad=18
+Estatura=1.58
+peso=52
+
+print(mensaje, "mi nombre es:",nombre, "soy de la formacion:",formacion, "tengo:",Edad, "años","mido:",Estatura, "y peso:",peso, "kg")
+#forma de imprimir 2
+print(f"{mensaje}. Mi nombre es: {nombre}, soy de la formacion {formacion}, tengo: {Edad} años, mido: {Estatura}, y peso: {peso} kg")
+
+A=5
+B=6
+suma= A + B
+resta= A - B
+Multiplicación= A * B
+División_conciente= A // B
+División_residuo= A % B
+print("la suma es:", suma, " la resta es:", resta, "la multiplicacion es:", Multiplicación, "la División cociente es:", División_conciente, "la Divion residuo:", División_residuo )
+print(f"la suma es: {suma}, la resta: {resta}, la multiplicación: {Multiplicación}, División_conciente: {División_conciente}, División_residuo: {División_residuo}" )
+
+A=int(input("digita un numero"))
+B=int(input("digita un numero"))
+
